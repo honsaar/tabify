c92fcd5dc195e14b13b80b44537b7a111f67c3cc
Aaron Shannon-Honson <aaron.shannon-honson@adelaide.edu.au>
2019-01-09 15:44:02 +1030

    Added basic styling and a butt to log in to Spotify auth

--- app.js
+++ app.js
@@ -6,5 +6,9 @@
 //client_secret = "56d630593c7a4c568b193de32e68d32b";
 //redirect_uri = "https://tabify.netlify.com/callback";
 
-//TODO: Make this an event listener from a login button -- to be done when a basic UI has been set up, perhaps
-window.location = "https://accounts.spotify.com/authorize?client_id=b8f474e0b2954346b6fad21c7e74a39c&redirect_uri=https:%2F%2Ftabify.netlify.com%2Fcallback&scope=user-read-private%20user-read-currently-playing%20user-read-playback-state%20user-read-email&response_type=token&state=123";
+window.onload = function () {
+    document.getElementById('loginButt').addEventListener('click', function (e) {
+            document.getElementById('loginButt').innerText = "Redirecting to Spotify Authentication...";
+            window.location = "https://accounts.spotify.com/authorize?client_id=b8f474e0b2954346b6fad21c7e74a39c&redirect_uri=https:%2F%2Ftabify.netlify.com%2Fcallback&scope=user-read-private%20user-read-currently-playing%20user-read-playback-state%20user-read-email&response_type=token&state=123";
+        });
+    };
--- callback.js
+++ callback.js
@@ -19,25 +19,27 @@ urlHash = parseURLHash(); //get the access token from the URL parameters
 var token = urlHash.access_token; // save to variable
 spotifyApi.setAccessToken(token); //set the access token in the API helper
 
-//DOM manipulation
-document.getElementById("app").innerHTML = "<p>Hello</p>"; 
-
 //Get the user's own data, returns an object
 window.onload = function () {
-    document.getElementById('myButtId').addEventListener('click', function (getSong) {
+    document.getElementById('myButtId').addEventListener('click', function (getSong) { //Question: What dis? Where is the getSong parameter coming from? We need use strict up in this shit
     spotifyApi.getMyCurrentPlayingTrack()
         .then(function (data) {
             console.log(data);
             document.getElementById("SongInfo").innerHTML = "";
-            document.getElementById("SongInfo").innerHTML += "<p>Trackname: <strong>" + data.item.name + "</strong></p>";
+            document.getElementById("SongInfo").innerHTML += "<p>Track name: <strong>" + data.item.name + "</strong></p>";
             document.getElementById("SongInfo").innerHTML += "<p>Artist: <strong>" + data.item.artists[0].name + "</strong></p>";
 
             var _searchString = data.item.name;
+            // adjust this for different services if required? 
+            //Do we need it as a comma-separated value or should we make it into an object where we can build our queries out ourselves using var song as an object - e.g.: song.name; song.artist[0], song.artist[1], and so on instead of a string we perform transforms on?
+
+            //also, how do we cover remastered versions of songs? 'All You Need is Love - Remastered, Beatles, The' as an example. How do we string match with the APIs? (this is likely not something we need to care about right now -- we don't know how the APIs work exactly yet so whatevs. Just some considerations)
+
             for (var i = 0; i < data.item.artists.length; i++) {
 
                 _searchString += ", " + data.item.artists[i].name;
             }
-            document.getElementById("SongInfo").innerHTML += "<p>" + _searchString + "</p>";
+            document.getElementById("SongInfo").innerHTML += "<p>Search string for API: <strong>" + _searchString + "</strong></p>";
 
         }, function (err) {
             console.error(err);
