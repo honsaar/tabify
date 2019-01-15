3c94f8a0f8eef9e2419a3e750f3a4ed99b434368
Andrew Martin <Janmebows@gmail.com>
2019-01-15 15:49:08 +1030

    Probably would help if i didn't mismatch brackets

--- callback.js
+++ callback.js
@@ -22,8 +22,8 @@ var token = urlHash.access_token; // save to variable
 spotifyApi.setAccessToken(token); //set the access token in the API helper
 
 //Get the user's own data, returns an object
-window.onload = function () {
-    document.getElementById('myButtId').addEventListener('click', function () { //Question: What dis? Where is the getSong parameter coming from? We need use strict up in this shit
+//window.onload = function () {
+document.getElementById('myButtId').addEventListener('click', function () {
     spotifyApi.getMyCurrentPlayingTrack()
         .then(function (data) {
             console.log(data);
@@ -47,7 +47,6 @@ window.onload = function () {
         }, function (err) {
             console.error(err);
         });
-
 });
 
 
@@ -61,5 +60,5 @@ spotifyApi.getMe()
   }, function(err) {
     console.error(err);
   });
-}
+
 
