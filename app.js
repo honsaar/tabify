a44f0fdb93eb1fbd767d8e58e786a1e42ac4a06b
Andrew Martin <Janmebows@gmail.com>
2019-01-16 10:52:59 +1030

    test adding electron

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
 //FOR REFERENCE -- coded into URL in Window.Location call.
 //client_id = "b8f474e0b2954346b6fad21c7e74a39c";
 //client_secret = "56d630593c7a4c568b193de32e68d32b";
-//redirect_uri = "https://tabify.netlify.com/callback";
+//OLD redirect_uri = "https://tabify.netlify.com/callback";
+//NEW redirect_uri = ""
 
 window.onload = function () {
     document.getElementById('loginButt').addEventListener('click', function (e) {
